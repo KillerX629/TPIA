1368db06d1dab1955f6fc54ad24ec3121978957c
Ignacio Pita <33305389+KillerX629@users.noreply.github.com>
2024-05-07 20:15:00 -0300

    ambiente funcionando: cartpole-v1 con 600 iteraciones y mostrando el aprendizaje en un gráfico

--- model.py
+++ model.py
@@ -1,19 +1,22 @@
 import torch
 import torch.nn as nn
-import torch.optim as optim
-
+import torch.nn.functional as F
 
 
 class NeuralNetwork(nn.Module):
     def __init__(self, input_dim, hidden_dim, hidden_layers, output_dim):
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.hidden_layers = hidden_layers
+        self.output_dim = output_dim
         super(NeuralNetwork, self).__init__() 
         self.layers = nn.ModuleList()
         self.layers.append(nn.Linear(input_dim, hidden_dim))
-        
+
         # Add hidden layers
         for _ in range(hidden_layers - 1):
             self.layers.append(nn.Linear(hidden_dim, hidden_dim))
-        
+
         self.layers.append(nn.Linear(hidden_dim, output_dim))
 
     def forward(self, x): 
@@ -21,3 +24,23 @@ class NeuralNetwork(nn.Module):
             x = torch.relu(layer(x))
         x = self.layers[-1](x) # Output layer
         return x
+
+
+class DQN(nn.Module):
+
+    def __init__(self, n_observations, n_actions, n_hidden=128, n_layers=2):
+        self.n_layers=n_layers
+        super(DQN, self).__init__()
+        self.layer1 = nn.Linear(n_observations, 128)
+        for i in range(n_layers - 1):
+            setattr(self, f"layer{i+2}", nn.Linear(128, 128))
+        setattr(self, f"layer{n_layers+1}", nn.Linear(128, n_actions))
+
+    # Called with either one element to determine next action, or a batch
+    # during optimization. Returns tensor([[left0exp,right0exp]...]).
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        for i in range(2, self.n_layers + 1):
+            x = F.relu(getattr(self, f"layer{i}")(x))
+        
+        
